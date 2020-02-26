06ce5b9a8e515a613f1d1b5dc4d8786aa041a422
Jay Barr <jayrbarr@gmail.com>
2020-02-26 09:48:23 -0500

    Solves 6. Integer Range

--- src/recursion.js
+++ src/recursion.js
@@ -39,6 +39,7 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+    return (Math.abs(x-y) > 1) ? ( x < y) ? [].concat([x+1], range(x+1,y)) : [].concat([x-1], range(x-1,y)) : [];
 };
 
 // 7. Compute the exponent of a number.
